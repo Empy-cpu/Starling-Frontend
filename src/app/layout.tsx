61fad3fe10b46da9eb64f5eb9ebdc16cebda8d4d
Firesky57 <82162170+Empy-cpu@users.noreply.github.com>
2025-09-22 12:53:36 +0500

    Added GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 // src/app/layout.tsx
 
 import type { Metadata } from "next";
+import Script from 'next/script';
 import LayoutWrapper from "./layout-wrapper";
-import ThemeRegistry from "@/Components/ThemeRegistry/ThemeRegistry"; 
-import GoogleAnalytics from "@/Components/Analytics/GoogleAnalytics";
+import ThemeRegistry from "@/Components/ThemeRegistry/ThemeRegistry";
 import "./globals.css";
+
+const GTM_ID = 'GTM-P4CV7HDL';
 
 export const metadata: Metadata = {
   title: "Starling Cleaners",
@@ -14,19 +16,37 @@ export const metadata: Metadata = {
   },
 };
 
-// Add this at the top of your file with other imports
-import { Suspense } from 'react';
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <Suspense>
-          <GoogleAnalytics />
-        </Suspense>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
       </head>
       <body suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        
         <ThemeRegistry>
           <LayoutWrapper>{children}</LayoutWrapper>
         </ThemeRegistry>
